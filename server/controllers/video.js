16fb50f7d39944b8d2a913755c52ad6bc61d1385
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2022-07-25 23:59:35 +0200

    I made some changes and added more functionality

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,4 +1,55 @@
-export const videos = (req, res, next) => {
-    res.json('its successfull')
-  };
-  
+export const addVideo = async (req, res, next) => {
+  const newVideo = new Video({ userId: req.user.id, ...req.body });
+  try {
+    const savedVideo = await newVideo.save();
+    res.status(201).json(newVideo);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) {
+      return next(createError(404, 'Video not found'));
+      if(req.user.id === video.userId) {
+        const updatedVideo = await Video.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+      res.status(200).json(updatedVideo);
+    }
+    else {
+      return next(createError(403, 'You are not authorized to update this video'));
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) {
+      return next(createError(404, 'Video not found'));
+      if(req.user.id === video.userId) {
+        const updatedVideo = await Video.findByIdAndDelete(req.params.id, { new: true });
+      res.status(200).json("The video has been deleted");
+    }
+    else {
+      return next(createError(403, 'You can not delete this video'));
+    }
+  } catch (err) {
+    next(err);
+  }
+}
+
+export const getVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) {
+      return next(createError(404, 'Video not found'));
+    }
+    res.status(200).json(video);
+  } catch (err) {
+    next(err);
+  }
+}
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,14 @@
 import express from 'express';
-import {  } from '../controllers/video.js';
+import { addVideo } from '../controllers/video.js';
+import { verifyToken } from '../verifyToken.js';
 
 const router = express.Router();
+
+// create video controller
+router.post('/', verifyToken, addVideo);
+router.put('/:id', verifyToken, updateVideo);
+router.delete('/:id', verifyToken, deleteVideo);
+router.get('/find/:id', getVideo);
+router.put('/view/:id', getVideo);
 
 export default router;
